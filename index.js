afe9507708df3f3334a233d6c0491de31e0ed31a
wissemayadi <wissem-ayadi@hotmail.com>
2021-09-16 23:37:59 +0100

    first remote rest api

---
+++ index.js
@@ -0,0 +1,50 @@
+const express=require("express"); // require express 
+const request= require("request-promise");
+const app=express(); // assign app to express server
+app.use(express.json()); // parse result in json 
+const port =process.env.port || 5000; // port : 5000
+
+
+
+//@http://localhost:5000/test/
+
+app.get("/test/:number",(req,res,next)=>{
+const {number}=req.params;
+
+
+    
+
+for (let i=1;i<101;i++){
+
+    if (number%15 ===0){
+  
+ 
+   return res.status(200).json({status: 0, message: "FIZZBUZZ'"}); 
+   
+    }else if (number % 3 ===0){
+        return res.status(200).json({status: 0, message: "FIZZ"});
+       
+    }else if (number % 5 === 0){
+        return res.status(200).json({status: 0, message: "BUZZ'"});
+    }else{
+        return res.status(200).json({status: 1, number});
+    }
+}
+
+next();
+})
+
+// runing the server on port : 5000 : you must on tippe on terminal  "nodemon server"
+app.listen(port,(req,res)=>{
+
+console.log(`the port is listenning on ${port}`)
+
+})
+
+
+
+/// exemple when you put this url in the browser : http://localhost:5000/test/15 : the output will be :
+// {
+// "status": 0,
+// "message": "FIZZBUZZ'"
+// } 
